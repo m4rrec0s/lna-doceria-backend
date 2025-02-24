06fdc8a07ca119de5d11bdf05b8d9a0d831330b2
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2025-02-24 15:27:18 -0300

    feat: replace key file with environment variables for Google Drive authentication

--- src/config/googleDriveConfig.ts
+++ src/config/googleDriveConfig.ts
@@ -1,8 +1,22 @@
 import { google } from "googleapis";
 import { Readable } from "stream";
 
+const credentials = {
+  type: process.env.GOOGLE_TYPE,
+  project_id: process.env.GOOGLE_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_PRIVATE_KEY,
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLIENT_ID,
+  auth_uri: process.env.GOOGLE_AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
+};
+
 const auth = new google.auth.GoogleAuth({
-  keyFile: "./credentials.json",
+  credentials,
   scopes: ["https://www.googleapis.com/auth/drive"],
 });
 const drive = google.drive({ version: "v3", auth });
